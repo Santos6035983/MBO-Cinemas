d7e1a5367423923be3e71753fad4320407efa366
Santos6035983 <6035983@mborijnland.nl>
2025-01-25 00:04:47 +0100

    commit

--- Sprint 5/Website/Home.php
+++ Sprint 5/Website/Home.php
@@ -4,18 +4,18 @@
     <section id="films">
         <h2>Nu in de bioscoop</h2>
         <article>
-            <img src="images\Films\Indiana Jones and the Dial of Destiny.png" alt="Indiana Jones and the Dial of Destiny">
+        <a href="film.php?filmID=film1"><img src="images\Films\Indiana Jones and the Dial of Destiny.png" alt="Indiana Jones and the Dial of Destiny">
             <h3>Indiana Jones and the Dial of Destiny</h3>
             <p>De man met de hoed keert terug voor een laatste avontuur.
                 Indiana Jones and the Dial of Destiny brengt Harrison Ford terug in zijn iconische rol als onze favoriete archeoloog op het scherm.</p>
         </article>
         <article>
-            <img src="images\Films\No Time To Die.png" alt="No Time To Die">
+        <a href="film.php?filmID=film2"><img src="images\Films\No Time To Die.png" alt="No Time To Die">
             <h3>No Time To Die</h3>
             <p>In de 25ste James Bond-film No Time To Die heeft James Bond zijn turbulente leven als geheim agent achter zich gelaten en leidt hij een rustig bestaan op Jamaica.</p>
         </article>
         <article>
-            <img src="images\Films\Oppenheimer.png" alt="Oppenheimer">
+        <a href="film.php?filmID=film3"><img src="images\Films\Oppenheimer.png" alt="Oppenheimer">
             <h3>Oppenheimer</h3>
             <p>Oppenheimer, geschreven en geregisseerd door Christopher Nolan, is een met IMAX®-camera’s gedraaide epische thriller die het fascinerende verhaal vertelt van de raadselachtige man die om de wereld te redden het risico moet nemen dat deze wordt verwoest.</p>
         </article>
