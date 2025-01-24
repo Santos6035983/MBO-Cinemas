36141973cd949aaa3b8fb22b5f92e0729bffa851
Santos6035983 <6035983@mborijnland.nl>
2025-01-24 20:14:10 +0100

    Commit

--- Sprint 5/Website/Login.php
+++ Sprint 5/Website/Login.php
@@ -19,6 +19,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['email'] = $user['email'];
         header('Location: Home.php');
         exit();
     } else {
--- Sprint 5/Website/Profiel.php
+++ Sprint 5/Website/Profiel.php
@@ -1,3 +1,17 @@
 <?php include 'lib\HeaderFooter\Header.php'; ?>
 
+<main>
+    <h2>Profiel</h2>
+    <p>Welkom, <?php echo $_SESSION['username']; ?>!</p>
+    <p>Uw profielgegevens:</p>
+    <ul>
+        <li>Gebruikersnaam: <?php echo $_SESSION['username']; ?></li>
+        <li>E-mail: <?php echo $_SESSION['email']; ?></li>
+    </ul>
+    <a href="Logout.php">Uitloggen</a>
+</main>
+
 <?php include 'lib\HeaderFooter\Footer.php'; ?>
+
+</body>
+</html>
